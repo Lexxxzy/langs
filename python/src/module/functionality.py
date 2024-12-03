12863bd302aeca057065ee9007c461093121b900
Lexxxzy <lex.halikov@yandex.ru>
2024-12-03 14:08:13 +0300

    Python new function

--- python/src/module/functionality.py
+++ python/src/module/functionality.py
@@ -13,7 +13,10 @@ class MyClass:
         self.instance_variable = instance_variable
 
     def display_instance_variable(self):
-        function_variable = "I am new function scope variable" 
+        function_variable = "I am new function scope variable"
+        def some_new_inner_function():
+            print(function_variable)
+        some_new_inner_function()
         print(f"Instance Variable Updated: {self.instance_variable}")
 
     @classmethod
@@ -28,3 +31,7 @@ class MyClass:
 def function_with_local_variable():
     local_variable = "I am a local variable"
     print(local_variable)
+
+def new_global_function():
+    new_variable = "Im new global function variable"
+    print(new_variable)
